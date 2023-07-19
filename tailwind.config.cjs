21bf59070e4820fe9884e0dc8db3e11cfb4013b7
senti <brianmv@uci.edu>
2023-07-18 21:53:20 -0700

    chore: restructure project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/** 
+ * @type {import('tailwindcss').Config}
+ */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,6 +16,5 @@ module.exports = {
     },
   },
   plugins: [
-    // ...
   ],
 }
