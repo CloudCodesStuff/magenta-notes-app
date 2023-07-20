b01691013558d6c0c2528e218c89d8a1afadd2e1
senti <brianmv@uci.edu>
2023-07-19 22:14:50 -0700

    fix: remove icon

--- src/components/ui/navigation-menu.tsx
+++ src/components/ui/navigation-menu.tsx
@@ -49,10 +49,12 @@ const NavigationMenuTrigger = UiForwardRef<typeof NavigationMenuPrimitive.Trigge
     >
       {children}
       {''}
+      {/*
       <ChevronDown
         className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
         aria-hidden="true"
       />
+      */}
     </NavigationMenuPrimitive.Trigger>
   ),
   {
