681141feff505dec02065964e45a15c8e09b04db
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-22 03:09:37 -0400

    fixed but with fixed userid TODO

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,34 +1,19 @@
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { getAllWorkspaces } from '@/lib/services/workspaces/get-all';
+import { trpc } from '@/lib/trpc'
+
 
 export const metadata = {
     title: "Dashboard",
 };
 
 export default function Dashboard() {
-    const { data: session } = useSession();
-    const [workspaces, setWorkspaces] = useState<{ id: number; name: string; createdAt: Date }[]>([]);
-
-    useEffect(() => {
-        // Fetch workspaces from the database and set them in the state
-        if (session?.user?.id) {
-            getAllWorkspaces(session.user.id)
-                .then((allWorkspaces) => {
-                    setWorkspaces(allWorkspaces);
-                })
-                .catch((error) => {
-                    console.error('Error fetching workspaces:', error);
-                });
-        }
-    }, [session]);
+    const workspaces = trpc.examplePrisma.getWorkspaces.useQuery()
 
     return (
         <div className="w-full">
             <div className="max-w-6xl mx-auto">
-                {workspaces.length ? (
+                {workspaces.data?.length ? (
                     <div className="divide-y divide-border rounded-md border">
-                        {workspaces.map((workspace) => (
+                        {workspaces.data.map((workspace) => (
                             <div key={workspace.id}>{workspace.name}</div>
                         ))}
                     </div>
--- src/server/routers/example-prisma.ts
+++ src/server/routers/example-prisma.ts
@@ -36,7 +36,7 @@ const examplePrismaRouter = router({
    * Get all workspaces in the database.
    */
   getWorkspaces: procedure.query(async () => {
-    const allWorkspaces = await getAllWorkspaces('id')
+    const allWorkspaces = await getAllWorkspaces('clkdjnxlk0000l608hsj9jxb2')
     return allWorkspaces
   }),
 })
