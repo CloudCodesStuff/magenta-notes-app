f0e6ec84bf337643a06e71bb87db4ab2bfa38c6a
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-22 01:38:07 -0400

    notespace attept 2

--- src/lib/services/workspaces/get-all.ts
+++ src/lib/services/workspaces/get-all.ts
@@ -3,7 +3,19 @@ import { db } from '@/lib/db'
 /**
  * Return all workspaces from the database.
  */
-export async function getAllWorkspaces() {
-  const allWorkspaces = await db.notespace.findMany()
+export async function getAllWorkspaces(id: string) {
+  const allWorkspaces = await db.notespace.findMany({
+    where: {
+      userId: id,
+    },
+    select: {
+      id: true,
+      name: true,
+      createdAt: true,
+    },
+    orderBy: {
+      updatedAt: 'desc',
+    },
+  })
   return allWorkspaces
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,68 +1,40 @@
-import { useSession } from 'next-auth/react'
-import { useEffect, useState } from 'react'; // Import useState and useEffect
-
-import { db } from '@/lib/db'
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { db } from '@/lib/db';
+import { getAllWorkspaces } from '@/lib/services/workspaces/get-all';
 
 export const metadata = {
     title: "Dashboard",
-}
-
-type Notespace = {
-    id: number;
-    name: string;
-    createdAt: Date;
 };
 
 export default function Dashboard() {
-    const { data: session, status } = useSession(); // Destructure `status` to check if it's "loading" or "error"
-    const [notespaces, setNotespaces] = useState<Notespace[]>([]); // State to store notespaces
+    const { data: session, status } = useSession();
+    const [workspaces, setWorkspaces] = useState<{ id: number; name: string; createdAt: Date }[]>([]);
 
-    // Move getNotes inside useEffect to run once the component mounts
     useEffect(() => {
-        async function getNotes() {
-            if (session?.user?.id) {
-                try {
-                    const notespaces = await db.notespace.findMany({
-                        where: {
-                            userId: session.user.id,
-                        },
-                        select: {
-                            id: true,
-                            name: true,
-                            createdAt: true,
-                        },
-                        orderBy: {
-                            updatedAt: "desc",
-                        },
-                    });
-                    setNotespaces(notespaces); // Update the state with the retrieved notes
-                } catch (error) {
-                    console.error("Error fetching notes:", error);
-                }
-            }
+        // Fetch workspaces from the database and set them in the state
+        if (session?.user?.id) {
+            getAllWorkspaces(session.user.id)
+                .then((allWorkspaces) => {
+                    setWorkspaces(allWorkspaces);
+                })
+                .catch((error) => {
+                    console.error('Error fetching workspaces:', error);
+                });
         }
-        getNotes();
-    }, [session]); // Run the effect whenever the session changes
-
-    if (status === "loading") {
-        return <div>Loading...</div>;
-    }
-
-    if (!session) {
-        return <div>Please log in to access this page.</div>;
-    }
+    }, [session]);
 
     return (
         <div className="w-full">
             <div className="max-w-6xl mx-auto">
-                {notespaces.length ? (
+                {workspaces.length ? (
                     <div className="divide-y divide-border rounded-md border">
-                        {notespaces.map((notespace) => (
-                            <div key={notespace.id}>{notespace.name}</div>
+                        {workspaces.map((workspace) => (
+                            <div key={workspace.id}>{workspace.name}</div>
                         ))}
                     </div>
                 ) : (
-                    <div>No notespaces found.</div>
+                    <div>No workspaces found.</div>
                 )}
             </div>
         </div>
