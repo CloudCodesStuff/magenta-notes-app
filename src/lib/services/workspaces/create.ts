18e337965ef13387c485e3929d2883bb34bc652c
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-22 14:18:36 -0400

    Build errors again

--- src/lib/services/workspaces/create.ts
+++ src/lib/services/workspaces/create.ts
@@ -1,11 +1,15 @@
 import { db } from '@/lib/db'
 import { z } from 'zod'
+import { createNoteInput } from '../notes/create'
 
 /**
- * Input for creating new workspace
- */
+ 
+Input for creating new workspace*/
 
-export const createWorkspaceInput = z.object({})
+export const createWorkspaceInput = z.object({
+  name: z.string(),
+  description: z.string(),
+})
 
 export async function createWorkspace(data: z.infer<typeof createWorkspaceInput>) {
   const workspace = await db.workspace.create({ data })
