3d334916c88919cf17700e886dce0fd149237f56
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-22 19:00:59 -0400

    Added google auth

--- env.ts
+++ env.ts
@@ -3,6 +3,8 @@ import { type } from 'arktype'
 const env = type({
   'GITHUB_ID?': 'string',
   'GITHUB_SECRET?': 'string',
+  'GOOGLE_ID?': 'string',
+  'GOOGLE_SECRET?': 'string',
 }).assert({ ...process.env })
 
 export default env
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,13 @@
 import type { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 import env from 'env'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from '@/lib/db'
-
+export const google = GoogleProvider({
+  clientId: env.GOOGLE_ID ?? '',
+  clientSecret: env.GOOGLE_SECRET ?? '',
+})
 export const github = GithubProvider({
   clientId: env.GITHUB_ID ?? '',
   clientSecret: env.GITHUB_SECRET ?? '',
@@ -13,7 +17,7 @@ export const authOptions: AuthOptions = {
   // I don't like Next-Auth because their codebase is abysmal, which results in issues like type inconsistencies.
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   adapter: PrismaAdapter(db) as any,
-  providers: [github],
+  providers: [github, google],
   callbacks: {
     async session({ token, session }) {
       if (token) {
