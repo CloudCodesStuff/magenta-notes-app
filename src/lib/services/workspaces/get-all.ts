336fbab8b5ee1d3e4ce2c82a33cc87d05ea2ffdd
CloudCodes <85653592+CloudCodesStuff@users.noreply.github.com>
2023-07-22 01:20:35 -0400

    fixed build errors*3

--- src/lib/services/workspaces/get-all.ts
+++ src/lib/services/workspaces/get-all.ts
@@ -3,7 +3,7 @@ import { db } from '@/lib/db'
 /**
  * Return all workspaces from the database.
  */
-export async function getAllNotespaces() {
-  const allNotespaces = await db.notespace.findMany()
-  return allNotespaces
+export async function getAllWorkspaces() {
+  const allWorkspaces = await db.notespace.findMany()
+  return allWorkspaces
 }
