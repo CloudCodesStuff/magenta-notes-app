0424c666a194a1c0b261ceb5e10af0c94f83e797
senti <brianmv@uci.edu>
2023-07-22 19:54:18 -0700

    fix: authenticated requests during ssr

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -19,7 +19,7 @@ function getBaseUrl() {
 }
 
 export const trpc = createTRPCNext<AppRouter>({
-  config() {
+  config(opts) {
     return {
       transformer,
       links: [
@@ -32,8 +32,13 @@ export const trpc = createTRPCNext<AppRouter>({
 
           // You can pass any HTTP headers you wish here
           async headers() {
+            if (!opts.ctx?.req?.headers) {
+              return {}
+            }
+            // To use SSR properly, you need to forward client headers to the server
+            // This is so you can pass through things like cookies when we're server-side rendering
             return {
-              // authorization: getAuthCookie(),
+              cookie: opts.ctx.req.headers.cookie,
             }
           },
         }),
